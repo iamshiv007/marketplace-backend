1febdfa17fadf168267270a55cbe43ea5431cd1a
iamshiv007 <iamshiv20032003@gmail.com>
2023-07-07 13:08:59 +0530

    database connected

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const dotenv = require('dotenv').config()
 
 app.use(cors())
 
+// ConnectDb
+const connectDb = require('./connection/connect')
+connectDb()
+
 const port = process.env.PORT || 6000
 
 app.listen(5000, () => {
     console.log(`Server is running on PORT http://localhost:${port}`)
+})
+
+// Test 
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Test Success" })
 })
