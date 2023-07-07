b57c70f27598a4115c4850eabb75baba2e1f0cfa
iamshiv007 <iamshiv20032003@gmail.com>
2023-07-07 19:25:33 +0530

    cookie update

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -16,8 +16,9 @@ exports.registerUser = async (req, res) => {
 
         const options = {
             expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-            httpOnly: true,
-            sameSite: 'None',
+            // httpOnly: true,
+            // sameSite: 'None',
+            // secure: false
         }
 
         res.cookie('myToken', token, options).status(201).json({ success: true, message: "User Created", user })
@@ -53,8 +54,9 @@ exports.loginUser = async (req, res) => {
 
         const options = {
             expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-            httpOnly: true,
-            sameSite: 'None',
+            // httpOnly: true,
+            // sameSite: 'None',
+            // secure: false
         }
 
         res.cookie('myToken', token, options).status(200).json({ success: true, message: "Logged in Successfully", user })
@@ -77,8 +79,9 @@ exports.logoutUser = (req, res) => {
     try {
         const options = {
             expires: new Date(Date.now()),
-            httpOnly: true,
-            sameSite: "None",
+            // httpOnly: true,
+            // sameSite: "None",
+            // secure: false
         }
 
         res.cookie('myToken', null, options).status(200).json({ success: true, message: "Logout Successfully" })
