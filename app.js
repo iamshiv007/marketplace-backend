72e551ab3ed5c9079c2784a1e664f21dd021d779
iamshiv007 <iamshiv20032003@gmail.com>
2023-07-07 18:40:35 +0530

    update

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const bodyParser = require("body-parser")
 app.use(express.json())
 app.use(cors({
     origin: ['http://localhost:3000', 'http://localhost:3001'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }))
 app.use(cookieParser())
