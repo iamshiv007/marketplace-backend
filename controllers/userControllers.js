a16e511ac476c31c1a50dd96a32f2a0771d90a37
iamshiv007 <iamshiv20032003@gmail.com>
2023-07-10 16:44:59 +0530

    get all users completed

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -91,3 +91,16 @@ exports.logoutUser = (req, res) => {
     }
 
 }
+
+// 5. All Users
+exports.getAllUsers = async (req, res) => {
+    try {
+
+        const users = await User.find()
+
+        res.status(200).json({ success: true, users })
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
-const { registerUser, loginUser, userDetails, logoutUser } = require('../controllers/userControllers')
+const { registerUser, loginUser, userDetails, logoutUser, getAllUsers } = require('../controllers/userControllers')
 const { isAuthenticatedUser } = require('../middleware/auth')
 
 router.route('/user/register').post(registerUser)
 router.route('/user/login').post(loginUser)
 router.route('/user/me').get(isAuthenticatedUser, userDetails)
+router.route('/user/all').get(getAllUsers)
 router.route('/user/logout').get(logoutUser)
 
 module.exports = router
